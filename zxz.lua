2a18c3a28a1e55693289382678a2c846cfc606da
xxshiheroxx <89813398+xxshiheroxx@users.noreply.github.com>
2026-08-11 13:36:18 -0300

    Update zxz.lua

--- zxz.lua
+++ zxz.lua
@@ -9,7 +9,7 @@ local white = Color3.fromRGB(255, 255, 255)
 local hairCol = Color3.fromRGB(255, 255, 255)
 local skinCol = Color3.fromRGB(255, 219, 172)
 
-local Key = "GOFUCKYOU"
+local Key = "LEUWIN"
 local HubKey = Enum.KeyCode.K
 
 -- ==================== KEY SYSTEM ====================
